8a2d386291c0f96ba4abcc1bffd645ba8d847457
Thallys Bezerra <bezerra.thallys@gmail.com>
2016-06-29 01:18:06 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
+//Slider effects
 $( document ).ready(function() {
     
-    $("header").addClass("fadeInDown"); // Top with fadeInDown
+    $("header").addClass("fadeInDown");
 
     setTimeout(function(){
-        $('.figureProfile').addClass("zoomInUp"); // imgProfile with zoomInUp
+        $('.figureProfile').addClass("zoomInUp");
     },500);
     setTimeout(function(){
-        $('.namePerson').addClass("bounceInLeft"); // h1.namePerson with bounceInLeft
+        $('.namePerson').addClass("bounceInLeft");
     },1000);
     setTimeout(function(){
-        $('.functionPerson').addClass("bounceInLeft"); // h2.functionPerson with bounceInLeft
+        $('.functionPerson').addClass("bounceInLeft");
     },1500);
     setTimeout(function(){
-        $('.infoPerson > p').addClass("bounceInLeft"); // h2.functionPerson with bounceInLeft
+        $('.infoPerson > p').addClass("bounceInLeft");
     },2000);
 
 });
@@ -27,33 +28,34 @@ $(".burgerMenu").click(function() {
     };
     // on odd clicks do this
     if (isEven(count) === false) {
-        $("header").addClass("fixedMenu"); // Fixed menu on top
-        $(".burgerMenu").addClass("showMenu"); // X button on menu
-        $(".linksMenu").addClass("showMenu"); // Open menu links
-        $(".navMenu").removeClass("bounceOutUp"); // Effect bounceOutUp on nav
-        $(".navMenu").addClass("bounceInDown"); // Effect bounceInDown on nav
-        $(".bgShade").removeClass("fadeOut"); // Cancel fffect fadeOut on shade
-        $(".bgShade").addClass("fadeIn"); // Start Effect fadeIn on shade
+        $("header").addClass("fixedMenu");
+        $(".burgerMenu").addClass("showMenu");
+        $(".linksMenu").addClass("showMenu");
+        $(".navMenu").removeClass("bounceOutUp");
+        $(".navMenu").addClass("bounceInDown");
+        $(".bgShade").removeClass("fadeOut");
+        $(".bgShade").addClass("fadeIn");
     }
     // on even clicks do this
     else if (isEven(count) === true) {
-        $("header").removeClass("fixedMenu"); // Remove fixed menu on top
-        $(".burgerMenu").removeClass("showMenu"); // Burger button on menu
-        $(".navMenu").removeClass("bounceInDown"); // Effect bounceInDown on nav
-        $(".navMenu").addClass("bounceOutUp"); // Effect bounceOutUp on nav
-        $(".bgShade").removeClass("fadeIn"); // Cancel effect fadeIn on shade
-        $(".bgShade").addClass("fadeOut"); // Start effect fadeOut on shade
+        $("header").removeClass("fixedMenu"); 
+        $(".burgerMenu").removeClass("showMenu");
+        $(".navMenu").removeClass("bounceInDown");
+        $(".navMenu").addClass("bounceOutUp");
+        $(".bgShade").removeClass("fadeIn");
+        $(".bgShade").addClass("fadeOut");
     }
 });
 
+// Animation on header
 var $anchor = $('html, body');
 $('.anchor').click(function() {
-    $("header").removeClass("fixedMenu"); // Remove fixed menu on top
-    $(".burgerMenu").removeClass("showMenu"); // Burger button on menu
-    $(".navMenu").removeClass("bounceInDown"); // Effect bounceInDown on nav
-    $(".navMenu").addClass("bounceOutUp"); // Effect bounceOutUp on nav
-    $(".bgShade").removeClass("fadeIn"); // Cancel effect fadeIn on shade
-    $(".bgShade").addClass("fadeOut"); // Start effect fadeOut on shade
+    $("header").removeClass("fixedMenu");
+    $(".burgerMenu").removeClass("showMenu");
+    $(".navMenu").removeClass("bounceInDown");
+    $(".navMenu").addClass("bounceOutUp");
+    $(".bgShade").removeClass("fadeIn");
+    $(".bgShade").addClass("fadeOut");
     count++;
     $anchor.animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
@@ -61,6 +63,12 @@ $('.anchor').click(function() {
     return false;
 });
 
+// Burger menu without one extra click
+$('.linkHeader').click(function() {
+    count++;
+});
+
+// Anchor to header
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
